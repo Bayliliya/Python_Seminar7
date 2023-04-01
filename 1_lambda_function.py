f7419ae3533143da1c986e5804ba7b4ecb3eb72f
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-04-01 14:16:51 +0300

    ДЗ-34

---
+++ 1_lambda_function.py
@@ -0,0 +1,12 @@
+# def get_number_string(num: int) -> str:
+#     """Функция возвращает строку с числом.
+
+#         Args:
+#         num (int): Число, которое необходимо вывести.
+
+#         Returns:
+#         str: Строка с числом.
+#         """
+#     return f"Твоё число {num}"
+
+print(lambda num: f"Твое число {num}")
