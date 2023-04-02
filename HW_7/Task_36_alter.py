572c210287e8af4a77f47f7cef105cdff79f7116
Liliya <105841191+Bayliliya@users.noreply.github.com>
2023-04-02 14:58:52 +0300

    Добавила аргументы

--- HW_7/Task_36_alter.py
+++ HW_7/Task_36_alter.py
@@ -11,25 +11,31 @@
 ровно два аргумента, как, например, у операции умножения.'''
 
 
-def operation(x, y):
+def operation(x_row, y_column):
     '''Функция вычисляет значение элемента'''
-    return x * y
-
-
-x_row = 6
-y_column = 6
-
-
-def print_operation_table(operation, x_row, y_column):
-    '''Функция вычисляет элемент по номеру строки и столбца'''
-
+    massiv_item = []
+    total_massiv = []
     for i in range(1, x_row + 1):
         for j in range(1, y_column + 1):
-            print(operation(i, j), end="  ")
-        print(" ")
+            massiv_item.append(i * j)
+        total_massiv.append(massiv_item)
+        massiv_item = []
+    return total_massiv
+
+
+x = 8
+y = 8
+
+
+def print_operation_table(func):
+    '''Функция вычисляет элемент по номеру строки и столбца'''
+    func = operation(x, y)
+    result_massiv = func
+    for _, num in enumerate(result_massiv):
+        print(*num)
 
 
 x = int(input("X: "))
 y = int(input("Y: "))
 
-print_operation_table(lambda x, y: x * y, x, y)
+print_operation_table(lambda x, y: x * y)
